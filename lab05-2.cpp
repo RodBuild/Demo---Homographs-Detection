51081d8eaa1a8457cb558fe68873c4bfbdea5f6b
Rodrigo Rodriguez <rod19016@byui.edu>
2022-05-17 14:39:52 -0600

    improved menu, more relevant comments

--- lab05-2.cpp
+++ lab05-2.cpp
@@ -19,6 +19,31 @@ const std::string SINGLE_DOT = ".";
 const std::string DOUBLE_DOT = "..";
 const std::string DRIVES[2] = {"C:", "D:"};
 
+
+/*************************
+ * Run example test case *
+ *************************/
+void runTestCases() {
+    cout << ":)\n";
+}
+
+/*************************************
+ * Determine if paths are homographs *
+ *************************************/
+bool areHomographs(const vector<string> target_path, const vector<string> secret_path) {
+    bool homographs = true;
+    for (int i = 0; i < secret_path.size(); i++) {
+        if (target_path[i] != secret_path[i]) {
+            homographs = false;
+            break;
+        }
+    }
+    return homographs;
+}
+
+/****************************************
+ * Create a canon token of a given path *
+ ****************************************/
 vector<string> canonicalize(const std::string p) {
     // delete first '/'
     string path = p;
@@ -26,7 +51,7 @@ vector<string> canonicalize(const std::string p) {
         path.erase(0,1);
     }
 
-    vector<string> v_path, vb_path;
+    vector<string> vgiven_path, vbase_path;
     std::filesystem::path system;
     system = std::filesystem::current_path();
     string base_path{system.u8string()};
@@ -39,79 +64,104 @@ vector<string> canonicalize(const std::string p) {
             base_path[i] = SLASH;
         }
     }
+    //split base path and store in a vector
     stringstream ss(base_path);
     string dir;
     while (getline(ss, dir, SLASH))
-        vb_path.push_back(dir);
+        vgiven_path.push_back(dir);
 
-    //normalize given path
+    //split given path and store in a vector
     stringstream ss2(path);
     string dir2;
     while (getline(ss2, dir2, SLASH))
-        v_path.push_back(dir2);
-    
-    
-    //now cannonlize Union of base with path
-    // for (auto x: v_path)
-        // cout << x << endl;
+        vgiven_path.push_back(dir2);
 
-    // int location = vb_path.size() - 1;
-    for (auto it = v_path.begin(); it != v_path.end(); it++) {
+    //use given path to navigate base path
+    for (auto it = vgiven_path.begin(); it != vgiven_path.end(); it++) {
         if (*it == SINGLE_DOT) {
             //do nothing
         }
         else if (*it == DOUBLE_DOT) {
-            vb_path.pop_back();
+            vbase_path.pop_back();
         }
         else {
-            vb_path.push_back(*it);
+            vbase_path.push_back(*it);
         }
 
-        //if empty at some point -> return the vector to show error
-        if (vb_path.empty()) {
-            return vb_path;
+        //if base path empty at some point -> return the vector to show error
+        if (vbase_path.empty()) {
+            return vbase_path;
         }
     }
 
-    for (auto x: vb_path)
-        cout << x << "/";
+    /*DEBUG*/
+    // display ultimate path (given path + base path)
+    // for (auto x: vbase_path) {
+    //     cout << x << "/";
+    // }
+    // cout << endl;
 
-    cout << endl;
+    return vbase_path;
+}
 
-    return vb_path;
+/************************
+ * Display Instructions *
+ ************************/
+void displayMenu() {
+    cout << "\n ----------------------------\n";
+    cout << "|     Lab 05: Homographs     |\n";
+    cout << "|----------------------------|\n";
+    cout << "|  (1) Run Test Cases        |\n";
+    cout << "|  (2) Manually Excute Test  |\n";
+    cout << "|  (3) Quit Program          |\n";
+    cout << " ----------------------------\n";
 }
 
 int main() {
-    string path1, path2;
-    cout << "Path 1: ";
-    getline(cin, path1);
-    cout << "Path 2: ";
-    getline(cin, path2);
-    // std::filesystem::path system;
-    // system = std::filesystem::current_path();
-    // string base_path{system.u8string()};
 
-    vector<string> target_path = canonicalize(path1);
-    vector<string> secret_path = canonicalize(path2);
+    string path1, path2, instruction;
 
-    if (target_path.empty())
-        cout << "PATH1 empty error";
+    bool in_use = true;
+    while (in_use) {
+        // wait for user's instruction
+        displayMenu();
+        cout << "> ";
+        getline(cin,instruction);
+        cout << endl;
 
-    if (secret_path.empty())
-        cout << "PATH2 empty error";
+        // run test cases
+        if (instruction == "1") {
+            runTestCases();
+        }
+        // run manual test
+        else if (instruction == "2") {
+            cout << "First file path: ";
+            getline(cin, path1);
+            cout << "Second file path: ";
+            getline(cin, path2);
 
-    bool homographs = true;
-    for (int i = 0; i < secret_path.size(); i++) {
-        if (target_path[i] != secret_path[i]) {
-            homographs = false;
+            vector<string> target_path = canonicalize(path1);
+            vector<string> secret_path = canonicalize(path2);
+
+            if (target_path.empty())
+                cout << "PATH1 empty error";
+            else if (secret_path.empty())
+                cout << "PATH2 empty error";
+            else if (areHomographs(target_path, secret_path))
+                cout << "> The paths are homographs\n";
+            else
+                cout << "> The paths are not homographs\n";   
+        }
+        // quit program
+        else if (instruction == "3") {
+            cout << "> Have a great day!\n";
             break;
         }
-     }
-     if (homographs)
-        cout << "Homographs";
-    else if (!homographs)
-        cout << "No homographs";
-
+        // invalid command
+        else {
+            cout << "> ## INVALID COMMAND ##\n";
+        }
+    }
 
     return 0;
 }
